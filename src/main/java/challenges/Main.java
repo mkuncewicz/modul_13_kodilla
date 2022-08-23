887f046da24b6b93bd983557776f9807ea27c985
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-08-23 22:19:47 +0200

    first commit

---
+++ src/main/java/challenges/Main.java
@@ -0,0 +1,18 @@
+package challenges;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        MovieStore movieStore = new MovieStore();
+
+        Map<String, List<String>> list = movieStore.getMovies();
+
+
+
+
+    }
+}
