21757f929e6fc2a32fe356fad9acc7d1e853d57b
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-09-10 16:59:37 +0200

    module 14.2 taskv2

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,10 +1,11 @@
 package com.kodilla.spring.calculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
-@Service
+@Repository
 public final class Calculator {
+
     @Autowired
     private Display display;
 
@@ -12,18 +13,19 @@ public final class Calculator {
         display.displayValue(a+b);
         return a+b;
     }
+
     public double sub(double a, double b){
         display.displayValue(a-b);
         return a-b;
     }
+
     public double mul(double a, double b){
         display.displayValue(a*b);
         return a*b;
     }
+
     public double div(double a, double b){
         display.displayValue(a/b);
         return a/b;
     }
-
-
 }
--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -9,3 +9,4 @@ public class Display {
         System.out.println("Result: "+ val);
     }
 }
+
