8bdbc40853c957a42a8c87dd4bf7042d1f5b78d8
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-09-11 19:54:22 +0200

    module 14.3l

--- kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
@@ -19,4 +19,14 @@ class ReaderTestSuite {
         //When & Then
         reader.read();
     }
+
+    @Test
+    void testConditional() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(ReaderConfig.class);
+        //When
+        boolean book2Exists = context.containsBean("book2");
+        //Then
+        System.out.println("Bean book2 was found in the container: " + book2Exists);
+    }
 }
