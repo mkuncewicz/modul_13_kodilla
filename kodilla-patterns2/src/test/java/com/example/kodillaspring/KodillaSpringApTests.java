76a003311459469161b94ab23e4b87c70cd6993f
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-11-09 10:25:34 +0100

    Wszystko

---
+++ kodilla-patterns2/src/test/java/com/example/kodillaspring/KodillaSpringApTests.java
@@ -0,0 +1,13 @@
+package com.example.kodillaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KodillaSpringApTests {
+
+    @Test
+    void context(){
+        
+    }
+}
